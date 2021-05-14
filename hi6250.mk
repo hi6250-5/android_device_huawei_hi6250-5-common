59b7a3a226c1fe47f9f45defd576ea1e050868f1
dianlujitao <dianlujitao@lineageos.org>
2021-05-14 16:56:58 +0200

    hi6250: Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    Signed-off-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: Id59369186474fa4db311e4c3dfdf853c9b1450a1

--- hi6250.mk
+++ hi6250.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.hi6250 \
