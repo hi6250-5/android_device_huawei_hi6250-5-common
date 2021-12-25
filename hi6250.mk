17686996e2265516b1995691fad54983e7fa0694
A2L5E0X1 <alexed25@protonmail.com>
2021-12-25 22:35:00 +0100

    hi6250: import bcm's default wifi overlay configs
    Change-Id: I635990360b4504db69262adb3cf5a9df453c7803

--- hi6250.mk
+++ hi6250.mk
@@ -180,3 +180,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
