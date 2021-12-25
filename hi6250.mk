3bccaf2ed4e614e11de1a1602a4a320f9acc6ae0
A2L5E0X1 <alexed25@protonmail.com>
2021-12-25 20:29:11 +0100

    hi6250: copy google's media codec xmls
    Change-Id: I7b9362e5453c872453f821ba3a12ec7ee596d38e

--- hi6250.mk
+++ hi6250.mk
@@ -97,6 +97,12 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
