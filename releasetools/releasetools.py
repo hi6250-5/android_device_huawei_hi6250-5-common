a2a6202f4422fd870eaaa70c4fc3068c3948ea7a
A2L5E0X1 <alexed25@protonmail.com>
2021-12-26 20:23:14 +0100

    hi6250: block twrp from installing this rom
    * We don't want to support TWRP and it's data fuckups.
    
    Change-Id: I918ca6f349e7c9d2f652b37086970800651abe60

---
+++ releasetools/releasetools.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#
+# This releasetools extension blocks TWRP from installing this ROM.
+#
+
+import common
+import re
+
+def FullOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
+
+def OTA_Assertions(info):
+  info.script.AppendExtra('assert(getprop("ro.twrp.boot") == "" || abort("ERROR: TWRP detected! Please use provided Lineage Recovery! For more info, read t.me/hi6250_oss/12."););')
+  return
