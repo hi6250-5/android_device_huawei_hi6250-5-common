10c5c4c928f28ab0d610c7f604f078f8f38f9148
R0rt1z2 <rogerortizleal@gmail.com>
2021-12-29 17:13:28 +0100

    hi6250: link ui shim to camera algo lib
    Change-Id: I919b3ed78b6139ac21befc1c112efea5264df343

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,7 @@ TARGET_LD_SHIM_LIBS := \
     /vendor/lib/soundfx/libhuaweiprocessing.so|libshim_icu.so \
     /vendor/lib/libcamera_algo.so|libshim_algo.so \
     /vendor/lib/libcamera_algo.so|libshim_memtrack.so \
+    /vendor/lib/libcamera_algo.so|libshim_ui.so \
     /vendor/lib/libgnss_sensors_hisi.so|libshim_sensors.so \
     /vendor/lib/libhwuibp.so|libshim_renderer.so \
     /vendor/lib/libimonitor.so|libshim_log.so \
@@ -111,6 +112,7 @@ TARGET_LD_SHIM_LIBS := \
     /vendor/lib64/hw/fingerprint.hi6250.so|libshim_log.so \
     /vendor/lib64/libcamera_algo.so|libshim_algo.so \
     /vendor/lib64/libcamera_algo.so|libshim_memtrack.so \
+    /vendor/lib64/libcamera_algo.so|libshim_ui.so \
     /vendor/lib64/libgnss_sensors_hisi.so|libshim_sensors.so \
     /vendor/lib64/libhwuibp.so|libshim_renderer.so \
     /vendor/lib64/libimonitor.so|libshim_log.so \
